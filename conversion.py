aa380e1f02e62a5da35cf981bec6bbad8d7739fe
Tina Guan <qguan@uw.edu>
2018-04-17 10:50:17 -0700

    implement dollars2cents function

---
+++ conversion.py
@@ -0,0 +1,13 @@
+#Conversion functions needed:
+
+#- Dollars to Cents
+#- Gallons to Liters
+#- Feet to Inches
+#- Degrees to Radians
+
+# My conversion Tools
+
+def dollars2cents(dollars):
+	cents = dollars * 100
+	return cents
+ 
