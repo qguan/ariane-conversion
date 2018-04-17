5016725f3ed7bf602458e62d8de0fec2b8236f67
Tina Guan <qguan@uw.edu>
2018-04-17 11:03:52 -0700

    add dollar conversion function

--- ps-convert.py
+++ ps-convert.py
@@ -7,6 +7,10 @@ def feet2inches(unit):
   inches = unit*12
   return inches
 
+def dollars2cents(dollars):
+        cents = dollars * 100
+        return cents
+
 def gallon2liter(g):
     l = g * 3.78541
     return l
